cdc39e4f0034a75a69f25237f532d7bb141f14a5
mdquynhhuong <mdquynhhuong@gmail.com>
2021-07-09 18:06:01 -0400

    first commit

---
+++ pseudocode.js
@@ -0,0 +1,14 @@
+// Movie Night
+
+// A landing page with the app HTMLHeadingElement, "Movie Night" and a message "What do you feel like today?"
+
+// A list of genre in a small form that prompts the user with genres of the movies ("action", "adventure", "thriller",...)
+// (Stretch) Promt user to provide a random year in text box 
+
+// Store the user selection in one variable (Stretch: 2 variables)
+
+// When user submit form, make a request to retrieve thrid party data from moviedb.org based on user pregerence to search for movie.
+
+// Display a random movie depending on the genre by using random function, rendering HTML elements for movie name, year, poster of movie and movie description
+
+//(Stretch) Stores all the movies in an object for the genre that are the same genre of user promt. Then loop through each data to see if it satisfies the year user has chosen. Then rendering HTML elements for movie name, year, poster of movie and movie description
